444f56d13f4fe191ecd92f29dcf74ca1ed14f0b3
yurison <yu_ri_son@naver.com>
2023-07-30 17:20:57 +0900

    postForEntity() 테스트

--- src/main/java/com/example/practice/controller/ServerApiController.java
+++ src/main/java/com/example/practice/controller/ServerApiController.java
@@ -1,20 +1,31 @@
 package com.example.practice.controller;
 
 import com.example.practice.dto.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/server")
 public class ServerApiController {
 
     @GetMapping("/hello")
-    public User hello(@RequestParam String name, @RequestParam int age) {
+    public User getHello(@RequestParam String name, @RequestParam int age) {
         User user = new User();
         user.setName(name);
         user.setAge(age);
+        return user;
+    }
+
+    @PostMapping("/user/{userId}/name/{userName}")
+    public User post(
+            @RequestBody User user,
+            @PathVariable int userId,
+            @PathVariable String userName
+    ) {
+        log.info("client req : {}", user);
+        log.info("userId : {}, userName : {}", userId, userName);
+
         return user;
     }
 
