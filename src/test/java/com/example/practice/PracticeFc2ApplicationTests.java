9c64b6c8b4b99fbc1470229b1f15488ec75a2032
yurison <yu_ri_son@naver.com>
2023-07-30 16:31:54 +0900

    server init

---
+++ src/test/java/com/example/practice/PracticeFc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeFc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
